6f030db265406e9a39c9bf96289fd326ff0d6b17
aigerim <110599316+aigerimqz@users.noreply.github.com>
2026-04-08 16:07:45 +0500

    some addings

--- project/src/test/java/kz/kbtu/project/controller/ArtifactController.java
+++ project/src/test/java/kz/kbtu/project/controller/ArtifactController.java
@@ -1,4 +1,15 @@
 package kz.kbtu.project.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PostMapping
+@PutMapping
+@GetMapping
 public class ArtifactController {
+    
+
 }
